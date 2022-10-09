eb3ddd834b1b87ce9d6862c27d63c95198aededf
Agua1101 <agua1101@sina.com>
2022-10-09 18:17:35 +0800

    10月9日 1.参加易找标后台数据报表原型讨论会。 2.和马老师、张静沟通采购人分类问题。 3.测试gerapy爬虫管理平台，修复出现的问题。 4.将编辑部门提出的易找标bug整理到excel表中。 5.帮编辑部门处理8月和11月的学校相关数据。

---
+++ gerapy/gerapy/projects/ccgp_gov_cn/ccgp_gov_cn/pipelines.py
@@ -0,0 +1,94 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from yzb_tools.yzb_db_connect import *
+from yzb_conf import config as conf
+from yzb_page_url_ex import SavePart
+worker = IdWorker(sst.DATACENTER_ID,sst.WORKER_ID,sst.SEQUENCE)
+import pymongo
+
+
+class CcgpGovCnPipeline():
+    def __init__(self):
+        # self.d_save = MySQL(host=spider.settings.get('host'), port=spider.settings.get('port'), user=spider.settings.get('user'), password=spider.settings.get('password'), db=spider.settings.get('db'),
+        #                charset=spider.settings.get('charset'))
+
+        self.d_save = MySQL(host=conf.host, port=conf.port, user=conf.user, password=conf.password, db=conf.db,
+                       charset=conf.charset)
+
+
+
+    def process_item(self, item, spider):
+        dict_url_save = {}
+        dict_html_save = {}
+
+        page_id = worker.get_id()
+        # 保存t_website_page
+        save_part = SavePart(d_save=self.d_save)
+        p_id = save_part.url_save_part(det_url=item['page_url'], mission_id='7', dict_url=dict_url_save,
+                                web_table='t_website_page_1',page_id=page_id)
+        if p_id:
+            page_id = p_id
+            # 如果page_url有重复要同时删除t_bid_html表中对应的数据
+            self.d_save.runSql_excute(f'delete from t_bid_html where page_id={page_id}')
+
+
+        # id = worker.get_id()
+        #
+        #
+        # notnull_dict(item['page_url'], dict_url_save, 'page_url')
+        # notnull_dict(str(id), dict_url_save, 'id')
+        # notnull_dict('7', dict_url_save, 'mission_id')
+        # self.d_save.insert('t_website_page',dict_url_save)
+
+        # 保存t_bid_html
+        notnull_dict(item['title'], dict_html_save, 'title')
+        notnull_dict(item['date'], dict_html_save, 'date')
+        notnull_dict(item['html'], dict_html_save, 'html')
+        notnull_dict(str(page_id), dict_html_save, 'page_id')
+        self.d_save.insert('t_bid_html', dict_html_save)
+        self.d_save.commit_insert()
+
+        print('数据保存成功')
+        return item
+
+
+    def close_spider(self,spider):
+        self.d_save.close_db()
+
+
+# class CcgpGovCnPipeline():
+#     def __init__(self):
+#         self.myclient = pymongo.MongoClient(host="39.107.25.250", port=27017, username="admin", password="caigou2003")
+#         self.db = self.myclient['bid_test']
+#         self.cols = self.db['yzb_test']
+#
+#
+#     def process_item(self, item, spider):
+#         # dict_url_save = {}
+#         # dict_html_save = {}
+#         # id = worker.get_id()
+#
+#
+#         self.cols.insert_one({'title':item['title'],
+#                               'date':item['date'],
+#                               'html':item['html'],
+#                               'website':'1',
+#                               })
+#
+#         # notnull_dict(item['page_url'], dict_url_save, 'page_url')
+#         # notnull_dict(str(id), dict_url_save, 'id')
+#         # notnull_dict('7', dict_url_save, 'mission_id')
+#
+#         # notnull_dict(item['title'], dict_html_save, 'title')
+#         # notnull_dict(item['date'], dict_html_save, 'date')
+#         # notnull_dict(item['html'], dict_html_save, 'html')
+#         # self.d_save.insert('t_bid_html', dict_html_save)
+#         # self.d_save.commit_insert()
+#
+#         print('数据保存成功')
+#         return item
---
+++ gerapy/gerapy/projects/ccgp_gov_cn/ccgp_gov_cn/spiders/ccgp_gov_cn_spider.py
@@ -0,0 +1,104 @@
+import scrapy
+import time
+from datetime import datetime, date, timedelta
+from yzb_tools.yzb_tag_extract import *
+from ..items import CcgpGovCnItem
+import yzb_ip_proxy
+
+
+class CcgpGovCnSpiderSpider(scrapy.Spider):
+    name = 'ccgp_gov_cn_spider'
+
+    def __init__(self,name=None, **kwargs):
+        kwargs.pop('_job')  # 删除job
+        super().__init__(name, **kwargs)
+        self.headers = {
+            # 'Cookie': 'JSESSIONID=EgPd86-6id_etA2QDV31Kks3FrNs-4gwHMoSmEZvnEktWIakHbV3!354619916; Hm_lvt_9f8bda7a6bb3d1d7a9c7196bfed609b5=1545618390; Hm_lpvt_9f8bda7a6bb3d1d7a9c7196bfed609b5=1545618390; td_cookie=2144571454; Hm_lvt_9459d8c503dd3c37b526898ff5aacadd=1545611064,1545618402,1545618414; Hm_lpvt_9459d8c503dd3c37b526898ff5aacadd=1545618495',
+            'Cookie': 'Hm_lvt_9f8bda7a6bb3d1d7a9c7196bfed609b5=1657159792; Hm_lvt_9459d8c503dd3c37b526898ff5aacadd=1658819877; JSESSIONID=O4I50r8VN81k8-VOQ_PueAuF1NDX2xOBJHgSvSe-rDpQYgd7UQ-e!1179140079; Hm_lpvt_9459d8c503dd3c37b526898ff5aacadd=1658827822; Hm_lpvt_9f8bda7a6bb3d1d7a9c7196bfed609b5=1658827872',
+            'Host': 'search.ccgp.gov.cn',
+            # 'referer': 'https://search.ccgp.gov.cn/bxsearch',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3141.8 Safari/537.36',
+            'keep-alive':'False',
+        }
+        self.url_tag = 'ccgp.gov.cn'
+
+
+    def get_date(self):
+        year = time.strftime('%Y', time.localtime(time.time()))
+        today = time.strftime(':%m:%d', time.localtime(time.time()))
+        yesterday = (date.today() + timedelta(days=-1)).strftime(":%m:%d")
+
+        start_time = str(year) + yesterday
+        end_time = str(year) + today
+
+        return start_time,end_time
+
+    def start_requests(self):
+        url = 'http://search.ccgp.gov.cn/bxsearch'
+
+
+        start_time, end_time = self.get_date()
+        for i in range(1,10):
+            params = {
+                'searchtype': '1',
+                'page_index': str(i),
+                'bidSort': '0',
+                'pinMu': '0',
+                'bidType': '0',
+                'kw': '',
+                'start_time': '2022:08:20',
+                'end_time': '2022:08:20',
+                'timeType': '6'
+            }
+            # headers = {
+            #     'Cookie': 'JSESSIONID=EgPd86-6id_etA2QDV31Kks3FrNs-4gwHMoSmEZvnEktWIakHbV3!354619916; Hm_lvt_9f8bda7a6bb3d1d7a9c7196bfed609b5=1545618390; Hm_lpvt_9f8bda7a6bb3d1d7a9c7196bfed609b5=1545618390; td_cookie=2144571454; Hm_lvt_9459d8c503dd3c37b526898ff5aacadd=1545611064,1545618402,1545618414; Hm_lpvt_9459d8c503dd3c37b526898ff5aacadd=1545618495',
+            #     # 'Cookie': 'UM_distinctid=16b882cb78b7f6-04ea86bf03217f-5a40201d-1fa400-16b882cb78c990; Hm_lvt_9f8bda7a6bb3d1d7a9c7196bfed609b5=1569467097,1569476101,1569479884,1569479974; Hm_lpvt_9f8bda7a6bb3d1d7a9c7196bfed609b5=1569480034; JSESSIONID=231sWvpmQV90biZXbmgczIedW5cI-y7ummVBIoxhOFix7JPNySGz!-1490526721; Hm_lvt_9459d8c503dd3c37b526898ff5aacadd=1569399637,1569400221,1569476103,1569480964; Hm_lpvt_9459d8c503dd3c37b526898ff5aacadd=1569481021',
+            #     'Host': 'search.ccgp.gov.cn',
+            #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3141.8 Safari/537.36'
+            # }
+
+
+            yield scrapy.FormRequest(url=url, headers=self.headers, formdata=params,callback=self.list_parse,dont_filter=True)
+
+
+
+    def list_parse(self, response):
+        item = CcgpGovCnItem()
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+        }
+        url_list = response.xpath('//ul[@class="vT-srch-result-list-bid"]//a/@href').getall()
+        for det_url in url_list:
+            if det_url.startswith('http'):
+                print(det_url)
+                item['page_url'] = det_url
+
+                yield scrapy.Request(url=det_url,headers=headers,callback=self.page_parse)
+
+
+    def page_parse(self,response):
+        item = CcgpGovCnItem()
+
+        det_html = response.text
+        det_url = response.url
+        print(det_url,'det_url')
+
+        # html = content_html_ex(det_html,self.url_tag,det_url)
+        # print(det_html,'html')
+
+        title = title_ex_total('', det_html)
+        print(title,'title')
+        date = date_ex(det_html, '')
+        print(date,'date')
+
+        item['title'] = title
+        item['html'] = det_html
+        item['date'] = date
+        item['page_url'] = det_url
+
+
+        yield item
+
+
+
+
